9b24e5a4870ced1606f1218950b020d119c23d04
vietnguyenhoangw <vietnguyenhoangw@gmail.com>
2025-04-13 21:15:27 +0700

    :apple: [upd] - fixed build error libs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type {NextConfig} from "next";
 import withTM from "next-transpile-modules";
 
-const withTranspile = withTM(["antd", "rc-util", "rc-motion"]);
+const withTranspile = withTM([
+    "antd",
+    "rc-util",
+    "rc-motion",
+    "@ant-design/icons-svg",
+]);
 
 const nextConfig: NextConfig = {
     reactStrictMode: true,
