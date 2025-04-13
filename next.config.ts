c9290af9edfd19a32b3d51a4c64b830ff90e6dfe
vietnguyenhoangw <vietnguyenhoangw@gmail.com>
2025-04-13 20:56:01 +0700

    :bug: [fix] - config build for antd

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type {NextConfig} from "next";
+import withTM from "next-transpile-modules";
+
+const withTranspile = withTM(["antd", "rc-util", "rc-motion"]);
 
 const nextConfig: NextConfig = {
     reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withTranspile(nextConfig);
